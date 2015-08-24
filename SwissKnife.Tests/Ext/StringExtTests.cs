74a7ec5750494c7b84181f0601b7a74e20ab7634
Gabriel <gabrielvdk@gmail.com>
2015-08-24 20:33:42 +0200

    Created the Test Project

---
+++ SwissKnife.Tests/Ext/StringExtTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using SwissKnife.Ext;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissKnife.Ext.Tests
+{
+    [TestFixture]
+    public class StringExtTests
+    {
+        public class FormatSafe
+        {
+            [Test]
+            public void Correct_format()
+            {
+                string result = "Test {0}".FormatSafe("value1");
+                Assert.AreEqual("Test value1", result);
+            }
+
+            [Test]
+            public void Insufficient_arguments()
+            {
+                string result1 = "Test {0}".FormatSafe();
+                Assert.AreEqual("Test {0} ()", result1);
+
+                string result2 = "Test {0} {1}".FormatSafe("value1");
+                Assert.AreEqual("Test {0} {1} (value1)", result2);
+
+                string result3 = "Test {0} {1} {2}".FormatSafe("value1", "value2");
+                Assert.AreEqual("Test {0} {1} {2} (value1, value2)", result3);
+            }
+
+            [Test]
+            public void Incorrect_format_string()
+            {
+                string result1 = "Test {0".FormatSafe();
+                Assert.AreEqual("Test {0 ()", result1);
+            }
+        }
+    }
+}
