df691f19c4f83d728a1efdce728bd93eba6c86b1
Danny <mr_dark@darkweb.nl>
2015-09-12 21:42:00 +0200

    Changed namespace of StringExtTests

--- SwissKnife.Tests/Ext/StringExtTests.cs
+++ SwissKnife.Tests/Ext/StringExtTests.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SwissKnife.Ext.Tests
+namespace SwissKnife.Tests.Ext
 {
     [TestFixture]
     public class StringExtTests
