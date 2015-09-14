ea6f1958553c85f6febb6da4d0594807f1e02960
Danny <mr_dark@darkweb.nl>
2015-09-14 19:57:10 +0200

    Added system extension method ThrowIfNull
    if passed object is null an ArgumentNullException is thrown.

---
+++ SwissKnife.Tests/Ext/SystemExtTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using SwissKnife.Ext;
+
+namespace SwissKnife.Tests.Ext
+{
+    [TestFixture]
+    public class SystemExtTests
+    {
+        [Test]
+        public void ThrowIfNull_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => SystemExt.ThrowIfNull(null));
+        }
+
+        [Test]
+        public void ThrowIfNull_NotNull()
+        {
+            var notNullObject = new object();
+            Assert.DoesNotThrow(() => notNullObject.ThrowIfNull());
+        }
+    }
+}
---
+++ SwissKnife/Ext/SystemExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwissKnife.Ext
+{
+    public static class SystemExt
+    {
+        /// <summary>
+        /// Check if passed object is null and throw <see cref="ArgumentNullException"/> if true.
+        /// </summary>
+        /// <param name="o">Object to check</param>
+        /// <param name="paramName">Optional parameter name</param>
+        /// <returns>False if not null; otherwise <see cref="ArgumentNullException"/> is thrown</returns>
+        public static void ThrowIfNull(this object o, string paramName = "")
+        {
+            if (o == null)
+            {
+                if (paramName.IsNullOrEmpty())
+                {
+                    paramName = "o";
+                }
+
+                throw new ArgumentNullException(paramName, "Argument is null");
+            }
+        }
+    }
+}
